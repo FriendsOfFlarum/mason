ff259be4bd1140f39ff236edacc4bd34864fadb3
Clark Winkelmann <clark.winkelmann@gmail.com>
2017-10-05 02:30:00 +0200

    Rename routes and optimize imports

--- src/Listeners/AddApiRoutes.php
+++ src/Listeners/AddApiRoutes.php
@@ -2,15 +2,7 @@
 
 namespace Flagrow\Mason\Listeners;
 
-use Flagrow\Mason\Api\Controllers\AnswerDeleteController;
-use Flagrow\Mason\Api\Controllers\AnswerOrderController;
-use Flagrow\Mason\Api\Controllers\AnswerStoreController;
-use Flagrow\Mason\Api\Controllers\AnswerUpdateController;
-use Flagrow\Mason\Api\Controllers\FieldDeleteController;
-use Flagrow\Mason\Api\Controllers\FieldIndexController;
-use Flagrow\Mason\Api\Controllers\FieldOrderController;
-use Flagrow\Mason\Api\Controllers\FieldStoreController;
-use Flagrow\Mason\Api\Controllers\FieldUpdateController;
+use Flagrow\Mason\Api\Controllers;
 use Flarum\Event\ConfigureApiRoutes;
 use Illuminate\Contracts\Events\Dispatcher;
 
@@ -23,18 +15,53 @@ class AddApiRoutes
 
     public function routes(ConfigureApiRoutes $routes)
     {
-        /**
-         * Admin side
-         */
-        $routes->post('/flagrow/mason/fields/order', 'flagrow.discussion-fields.api.fields.order', FieldOrderController::class);
-        $routes->get('/flagrow/mason/fields', 'flagrow.discussion-fields.api.fields.index', FieldIndexController::class);
-        $routes->post('/flagrow/mason/fields', 'flagrow.discussion-fields.api.fields.store', FieldStoreController::class);
-        $routes->patch('/flagrow/mason/fields/{id:[0-9]+}', 'flagrow.discussion-fields.api.fields.update', FieldUpdateController::class);
-        $routes->delete('/flagrow/mason/fields/{id:[0-9]+}', 'flagrow.discussion-fields.api.fields.delete', FieldDeleteController::class);
+        // Fields
+        $routes->post(
+            '/flagrow/mason/fields/order',
+            'flagrow.mason.api.fields.order',
+            Controllers\FieldOrderController::class
+        );
+        $routes->get(
+            '/flagrow/mason/fields',
+            'flagrow.mason.api.fields.index',
+            Controllers\FieldIndexController::class
+        );
+        $routes->post(
+            '/flagrow/mason/fields',
+            'flagrow.mason.api.fields.store',
+            Controllers\FieldStoreController::class
+        );
+        $routes->patch(
+            '/flagrow/mason/fields/{id:[0-9]+}',
+            'flagrow.mason.api.fields.update',
+            Controllers\FieldUpdateController::class
+        );
+        $routes->delete(
+            '/flagrow/mason/fields/{id:[0-9]+}',
+            'flagrow.mason.api.fields.delete',
+            Controllers\FieldDeleteController::class
+        );
 
-        $routes->post('/flagrow/mason/fields/{id:[0-9]+}/answers/order', 'flagrow.discussion-fields.api.answers.order', AnswerOrderController::class);
-        $routes->post('/flagrow/mason/fields/{id:[0-9]+}/answers', 'flagrow.discussion-fields.api.answers.create', AnswerStoreController::class);
-        $routes->patch('/flagrow/mason/answers/{id:[0-9]+}', 'flagrow.discussion-fields.api.answers.update', AnswerUpdateController::class);
-        $routes->delete('/flagrow/mason/answers/{id:[0-9]+}', 'flagrow.discussion-fields.api.answers.delete', AnswerDeleteController::class);
+        // Answers
+        $routes->post(
+            '/flagrow/mason/fields/{id:[0-9]+}/answers/order',
+            'flagrow.mason.api.answers.order',
+            Controllers\AnswerOrderController::class
+        );
+        $routes->post(
+            '/flagrow/mason/fields/{id:[0-9]+}/answers',
+            'flagrow.mason.api.answers.create',
+            Controllers\AnswerStoreController::class
+        );
+        $routes->patch(
+            '/flagrow/mason/answers/{id:[0-9]+}',
+            'flagrow.mason.api.answers.update',
+            Controllers\AnswerUpdateController::class
+        );
+        $routes->delete(
+            '/flagrow/mason/answers/{id:[0-9]+}',
+            'flagrow.mason.api.answers.delete',
+            Controllers\AnswerDeleteController::class
+        );
     }
 }
