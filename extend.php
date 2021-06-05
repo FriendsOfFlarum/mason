d6b555fe80c4382f4e4d52550988974aaf8a2419
David Wheatley <hi@davwheat.dev>
2021-06-05 12:37:43 +0100

    Update PHP to be 1.0 compatible

--- extend.php
+++ extend.php
@@ -20,9 +20,11 @@ return [
     (new Extend\Frontend('forum'))
         ->css(__DIR__ . '/resources/less/forum.less')
         ->js(__DIR__ . '/js/dist/forum.js'),
+
     (new Extend\Frontend('admin'))
         ->css(__DIR__ . '/resources/less/admin.less')
         ->js(__DIR__ . '/js/dist/admin.js'),
+
     (new Extend\Routes('api'))
         // Fields
         ->post('/fof/mason/fields/order', 'fof-mason.api.fields.order', Api\Controllers\FieldOrderController::class)
@@ -36,26 +38,32 @@ return [
         ->post('/fof/mason/fields/{id:[0-9]+}/answers', 'fof-mason.api.answers.create', Api\Controllers\AnswerStoreController::class)
         ->patch('/fof/mason/answers/{id:[0-9]+}', 'fof-mason.api.answers.update', Api\Controllers\AnswerUpdateController::class)
         ->delete('/fof/mason/answers/{id:[0-9]+}', 'fof-mason.api.answers.delete', Api\Controllers\AnswerDeleteController::class),
+
     (new Extend\Locales(__DIR__ . '/resources/locale')),
 
     (new Extend\ApiController(ShowForumController::class))
         ->addInclude('masonFields.suggested_answers')
         ->prepareDataForSerialization(LoadForumFieldsRelationship::class),
+
     (new Extend\ApiSerializer(ForumSerializer::class))
         ->hasMany('masonFields', FieldSerializer::class)
-        ->mutate(ForumAttributes::class),
+        ->attributes(ForumAttributes::class),
 
     (new Extend\ApiController(ListDiscussionsController::class))
         ->addInclude('masonAnswers.field'),
+
     (new Extend\ApiController(ShowDiscussionController::class))
         ->addInclude('masonAnswers.field'),
+
     (new Extend\ApiController(CreateDiscussionController::class))
         ->addInclude('masonAnswers.field'),
+
     (new Extend\ApiController(UpdateDiscussionController::class))
         ->addInclude('masonAnswers.field'),
+
     (new Extend\ApiSerializer(DiscussionSerializer::class))
         ->hasMany('masonAnswers', AnswerSerializer::class)
-        ->mutate(function (DiscussionSerializer $serializer, Discussion $discussion): array {
+        ->attributes(function (DiscussionSerializer $serializer, Discussion $discussion): array {
             $canSee = $serializer->getActor()->can('seeMasonAnswers', $discussion);
 
             if (!$canSee) {
@@ -68,6 +76,7 @@ return [
                 'canUpdateMasonAnswers' => $serializer->getActor()->can('updateMasonAnswers', $discussion),
             ];
         }),
+
     (new Extend\Model(Discussion::class))
         ->relationship('masonAnswers', function (Discussion $discussion) {
             return $discussion->belongsToMany(Answer::class, 'fof_mason_discussion_answer', 'discussion_id', 'answer_id')
