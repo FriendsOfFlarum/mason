9916a63359c4ce12d138f2130f7924f327bb4253
David Wheatley <hi@davwheat.dev>
2021-06-09 14:07:28 +0100

    Create resolve alias

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,3 +1,11 @@
 const config = require('flarum-webpack-config');
+const { merge } = require('webpack-merge');
+const path = require('path');
 
-module.exports = config();
+module.exports = merge(config(), {
+    resolve: {
+        alias: {
+            '@lib': path.resolve(__dirname, 'src/lib/'),
+        },
+    },
+});
