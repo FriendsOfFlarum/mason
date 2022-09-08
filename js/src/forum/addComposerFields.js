53eec596241da2650280ed14fb6bc780b32f2f4a
Clark Winkelmann <clark.winkelmann@gmail.com>
2022-09-08 02:40:59 +0200

    Use ComposerState fields to hold answer data

--- js/src/forum/addComposerFields.js
+++ js/src/forum/addComposerFields.js
@@ -4,8 +4,6 @@ import DiscussionComposer from 'flarum/common/components/DiscussionComposer';
 import FieldsEditor from './components/FieldsEditor';
 
 export default function () {
-    DiscussionComposer.prototype.masonAnswers = [];
-
     extend(DiscussionComposer.prototype, 'headerItems', function (items) {
         if (!app.forum.canFillMasonFields()) {
             return;
@@ -14,9 +12,9 @@ export default function () {
         items.add(
             'mason-fields',
             <FieldsEditor
-                answers={this.masonAnswers}
+                answers={this.composer.fields.masonAnswers || []}
                 onchange={(answers) => {
-                    this.masonAnswers = answers;
+                    this.composer.fields.masonAnswers = answers;
                 }}
                 ontagchange={(tags) => {
                     this.composer.fields.tags = tags;
@@ -26,11 +24,11 @@ export default function () {
     });
 
     extend(DiscussionComposer.prototype, 'data', function (data) {
-        if (!app.forum.canFillMasonFields()) {
+        if (!app.forum.canFillMasonFields() || !this.composer.fields.masonAnswers) {
             return;
         }
 
         data.relationships = data.relationships || {};
-        data.relationships.masonAnswers = this.masonAnswers;
+        data.relationships.masonAnswers = this.composer.fields.masonAnswers;
     });
 }
